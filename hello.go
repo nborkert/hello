96f630e9c337b54c5a88f6e487fd84ed7bc31f5b
Neal Borkert <ndborkedaunt@neal-borkerts-imac-3.local>
2014-08-24 16:09:44 -0400

    Modified comment only

--- hello.go
+++ hello.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+
+
+import (
+    "fmt"
+    "net/http"
+)
+
+type Hello struct{}
+
+func (h Hello) ServeHTTP(
+    w http.ResponseWriter,
+    r *http.Request) {
+    fmt.Fprint(w, "Hello,yo!")
+    // // fmt.Fprint(w, "Hello,yo!")
+    // fmt.Println("Hey")
+    
+    
+}
+
+func (h Hello) printHey() {
+	fmt.Println("Hey")
+}
 
 func main() {
-	fmt.Printf("Hello, there. Second test of golang.\n")
+    var h Hello
+    go h.printHey()
+   	http.ListenAndServe("localhost:4000", h)
+    
 }
